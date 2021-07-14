4ef1028e5e96d26e47167a76819c4bd2e5fec3ea
Khaled Adad <khaledadad@lewisu.edu>
2021-07-14 13:13:18 -0500

    stuck on looping over array

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-// const List = ({list}) => list.map(({objectID, ...item}) => <Item key={item.objectID} {...item}/>)
+// const Photos = ({ pics }) => />)
 
 // const Item = ({ title, url, author, num_comments, points }) => (
 //     <div>
@@ -17,11 +17,17 @@ import React from 'react'
 
 const Photos = ({pics}) => {
   console.log(pics)
+  const listItems = pics.map((pic) => 
+    <li key={pic.id}>
+      <img src={`https://farm5.staticflickr.com/${pic.secret}/${pic.id}_${pic.secret}.jpg`} alt="" />
+    </li>
+  )
   return (
     <div className="photo-container">
       <h2>Results</h2>
       <ul>
-        <li>
+        {/* {listItems} */}
+        {/* <li>
           <img src="https://farm5.staticflickr.com/4334/37032996241_4c16a9b530.jpg" alt="" />
         </li>
         <li>
@@ -32,7 +38,7 @@ const Photos = ({pics}) => {
         </li>
         <li>
           <img src="https://farm5.staticflickr.com/4425/36337012384_ba3365621e.jpg" alt="" />
-        </li>
+        </li> */}
         {/* <!-- Not Found --> */}
         <li className="not-found">
           <h3>No Results Found</h3>
