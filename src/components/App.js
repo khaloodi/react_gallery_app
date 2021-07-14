38b546ebdb57f7acee5b305868fa60dd696e453a
Khaled Adad <khaledadad@lewisu.edu>
2021-07-14 16:22:36 -0500

    Cats are displaying woohoo

--- src/components/App.js
+++ src/components/App.js
@@ -7,19 +7,11 @@ import apiKey from '../config'
 
 function App() {
   const [searchTerm, setSearchTerm] = React.useState('')
-  const [data, setData] = React.useState({hits: []})
+  const [data, setData] = React.useState([])
 
   const handleSearch = e => {
     setSearchTerm(e.target.value)
   }
-
-  // useEffect(async () => {
-  //   const result = await axios(
-  //     'https://hn.algolia.com/api/v1/search?query=redux',
-  //   );
- 
-  //   setData(result.data);
-  // });
 
   React.useEffect (() => {
     axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=cats&per_page=24&format=json&nojsoncallback=1`)
--- src/components/Photos.js
+++ src/components/Photos.js
@@ -1,25 +1,19 @@
 import React from 'react'
 
-// const Photos = ({ pics }) => />)
+/* <li>
+  <img src="https://farm5.staticflickr.com/4334/37032996241_4c16a9b530.jpg" alt="" />
+</li> */
 
-// const Item = ({ title, url, author, num_comments, points }) => (
-//     <div>
-//       <span>
-//         <a href={url}>{title}</a>
-//       </span>
-//       <span>{author}</span>
-//       <span>{num_comments}</span>
-//       <span>{points}</span>
-//     </div>
-// )
+// Cat picture url from flicker API:
+// https://live.staticflickr.com/65535/51293482889_ea3216fb4f_w.jpg
 
-// export default List
+// https://farm${pic.farm}.staticflickr.com/${pic.secret}/${pic.id}_${pic.secret}.jpg
 
 const Photos = ({pics}) => {
   console.log(pics)
   const listItems = pics.map((pic) => 
     <li key={pic.id}>
-      <img src={`https://farm5.staticflickr.com/${pic.secret}/${pic.id}_${pic.secret}.jpg`} alt="" />
+      <img src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_w.jpg`} alt="" />
     </li>
   )
   return (
@@ -27,18 +21,7 @@ const Photos = ({pics}) => {
       <h2>Results</h2>
       <ul>
         {listItems}
-        {/* <li>
-          <img src="https://farm5.staticflickr.com/4334/37032996241_4c16a9b530.jpg" alt="" />
-        </li>
-        <li>
-          <img src="https://farm5.staticflickr.com/4342/36338751244_316b6ee54b.jpg" alt="" />
-        </li>
-        <li>
-          <img src="https://farm5.staticflickr.com/4343/37175099045_0d3a249629.jpg" alt="" />
-        </li>
-        <li>
-          <img src="https://farm5.staticflickr.com/4425/36337012384_ba3365621e.jpg" alt="" />
-        </li> */}
+
         {/* <!-- Not Found --> */}
         <li className="not-found">
           <h3>No Results Found</h3>
