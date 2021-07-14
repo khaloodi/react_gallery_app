46a71a1728b1a8b36154a90b128637c6216dff02
Khaled Adad <khaledadad@lewisu.edu>
2021-07-14 13:17:35 -0500

    uncommented listItems in photos component

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -26,7 +26,7 @@ const Photos = ({pics}) => {
     <div className="photo-container">
       <h2>Results</h2>
       <ul>
-        {/* {listItems} */}
+        {listItems}
         {/* <li>
           <img src="https://farm5.staticflickr.com/4334/37032996241_4c16a9b530.jpg" alt="" />
         </li>
